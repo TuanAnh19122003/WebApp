e7df395bd9cb1e0e77c06f68bbfa53c9048c6de4
TuanAnh19122003 <gamethunghiem19122003@gmail.com>
2025-08-05 10:14:11 +0700

    Xong CRUD Role

--- backend/server.js
+++ backend/server.js
@@ -12,11 +12,11 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use('/uploads', express.static(path.join(__dirname, 'src', 'uploads')));
 
-//const apiRouter = require('./src/routes/index.routes');
+const apiRouter = require('./src/routes/index');
 
 app.use(cors());
 
-// app.use('/api', apiRouter);
+app.use('/api', apiRouter);
 app.get('/', (req, res) => {
     res.send('Hello world')
 })
